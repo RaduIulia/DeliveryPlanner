ae6605151746d328a47b88e2fae208d732e65461
RADU Iulia-Teodora <iulia.radu10@yahoo.com>
2022-05-30 20:55:52 +0300

    exit button only

--- src/main/java/org/example/panel/ControlPanel.java
+++ src/main/java/org/example/panel/ControlPanel.java
@@ -13,42 +13,10 @@ import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 public class ControlPanel extends JPanel{
     MainFrame frame;
     public ControlPanel(MainFrame frame) {
-        JButton save = new JButton("Save");
-        JButton load = new JButton("Load");
         JButton exit = new JButton("Exit");
-        save.addActionListener(this::save);
-        load.addActionListener(this::load);
         exit.addActionListener(this::exit);
         exit.setBackground(Color.RED);
-        save.setBackground(Color.GREEN);
-        load.setBackground(Color.CYAN);
-        add(save);
-        add(load);
         add(exit);
-    }
-    private void load(ActionEvent e) {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Specify a file to load from:");
-        if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-            File loadFile = fileChooser.getSelectedFile();
-            System.out.println("Loading file: " + loadFile.getAbsolutePath());
-            try {
-                BufferedImage image = ImageIO.read(loadFile);
-                // TODO
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-    }
-
-    private void save(ActionEvent e) {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Specify a file to save:");
-        if (fileChooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
-            File fileToSave = fileChooser.getSelectedFile();
-            System.out.println("Save as file: " + fileToSave.getAbsolutePath());
-            System.out.println("save");
-        }
     }
 
     private void exit(ActionEvent e) {
@@ -58,22 +26,6 @@ public class ControlPanel extends JPanel{
         saveFrame.setPreferredSize(new Dimension(400, 80));
         saveFrame.setLocation(530, 350);
         saveFrame.pack();
-        JLabel saveLabel = new JLabel("Do you want to save?");
-
-        JPanel savePanel = new JPanel();
-        JButton yesButton = new JButton("Yes");
-        JButton noButton = new JButton("No ");
-        yesButton.addActionListener(this::saveAndExit);
-        noButton.addActionListener(this::exitNoSave);
-        savePanel.add(saveLabel);
-        savePanel.add(yesButton);
-        savePanel.add(noButton);
-        saveFrame.add(savePanel);
-        try {
-            wait(10);
-        } catch (InterruptedException interruptedException) {
-            interruptedException.printStackTrace();
-        }
         System.exit(0);
     }
 
@@ -81,8 +33,4 @@ public class ControlPanel extends JPanel{
         System.exit(0);
     }
 
-    private void saveAndExit(ActionEvent e) {
-        this.save(e);
-        System.exit(0);
-    }
 }
